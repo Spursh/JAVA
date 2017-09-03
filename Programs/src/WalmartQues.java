bf4a569eedb3c6aa166a0130501a7ec7d4c314be
Spursh Ujjawal <spurshujjawal@gmail.com>
2017-09-03 02:20:32 -0700

    Walmart Question

---
+++ Programs/src/WalmartQues.java
@@ -0,0 +1,18 @@
+
+public class WalmartQues {
+	
+	public static void printing(int[] a) {
+		for(int i = 0; i < a.length; i++) {
+			for (int j = i; j < a.length; j++)
+				System.out.println(a[i]+"-"+a[j]);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {1, 2, 3};
+		printing(a);
+
+	}
+
+}
